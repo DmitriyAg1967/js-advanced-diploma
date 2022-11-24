163132ef92bcc97769865c92e397a2c901c96084
Dmitriy <aglerimov05@mail.ru>
2022-11-24 20:43:45 +0300

    Удаление лишних файдов

--- README.md
+++
@@ -1,3 +0,0 @@
-# js-advanced-diploma Demo
-
-[![Build status](https://ci.appveyor.com/api/projects/status/h0pnqgme4rakve36?svg=true)](https://ci.appveyor.com/project/DmitriyAg1967/js-advanced-diploma)
--- src/js/__test__/Character.test.js
+++
@@ -1,9 +0,0 @@
-import Character from '../Character';
-
-test('Ошибка при создании new Character', () => {
-  function result() {
-    const unit = new Character(1);
-    return unit;
-  }
-  expect(result).toThrow('нельзя создавать персонажа Character, так как это базовый класс');
-});
